683ecf7481a69132f51b35b753bd39362e5842f0
Jeff <jahuangi@yahoo.ca>
2015-08-20 09:14:26 -0700

    added questions controller, with new method

---
+++ day1/awesome_answers/app/controllers/questions_controller.rb
@@ -0,0 +1,7 @@
+class QuestionsController < ApplicationController
+
+  # the new action is th eone that is used by convention in Rails
+  # to display a form to create the record (in this case, question record)
+  def new
+  end
+end
